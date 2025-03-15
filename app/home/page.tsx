e8b2c1777777391bff0ca460697d9765868a93b5
rakaism <rakadianp@gmail.com>
2025-03-15 13:56:30 +0700

    1

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function home() {
+export default function Home() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
